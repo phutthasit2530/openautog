e5fa0c01cca659b88be040ff2a1dfacb0e9cab3a
MyGatherBk <49818403+MyGatherBk@users.noreply.github.com>
2019-09-12 01:52:24 +0700

    Update pirakit.sh

--- pirakit.sh
+++ pirakit.sh
@@ -114,54 +114,24 @@ service squid3 restart
 
 
 ## download script
-cd /usr/bin
-wget -q -O g "https://raw.githubusercontent.com/MyGatherBk/pirakit/master/menu"
-wget -q -O speedtest "https://raw.githubusercontent.com/MyGatherBk/aungwin/master/speedtest"
-wget -q -O b-user "https://raw.githubusercontent.com/MyGatherBk/aungwin/master/b-user"
+wget -O /usr/local/bin/menu "https://raw.githubusercontent.com/MyGatherBk/pirakit/master/menu"
+	chmod +x /usr/local/bin/menu
+	wget -O /usr/local/bin/Auto-Delete-Client "https://raw.githubusercontent.com/MyGatherBk/PURE/master/Auto-Delete-Client"
+	chmod +x /usr/local/bin/Auto-Delete-Client 
+	apt-get -y install vnstat
 
 
 # info
 clear
 echo "Autoscript Include:" | tee log-install.txt
 echo "===========================================" | tee -a log-install.txt
-echo ""  | tee -a log-install.txt
-echo "Service"  | tee -a log-install.txt
-echo "-------"  | tee -a log-install.txt
-echo "OpenSSH  : 22, 80"  | tee -a log-install.txt
-echo "Dropbear : 443, 143"  | tee -a log-install.txt
-echo "Squid3   : 8080, 3128 (limit to IP SSH)"  | tee -a log-install.txt
-echo "OpenVPN  : TCP 1194 (client config : http://$MYIP:85/client.ovpn)"  | tee -a log-install.txt
-echo "badvpn   : badvpn-udpgw port 7300"  | tee -a log-install.txt
-echo "nginx    : 81"  | tee -a log-install.txt
-echo ""  | tee -a log-install.txt
-echo "Script"  | tee -a log-install.txt
-echo "------"  | tee -a log-install.txt
-echo "menu (Senarai perintah)"  | tee -a log-install.txt
-echo "usernew (Membuat Akaun SSH)"  | tee -a log-install.txt
-echo "trial (Membuat Akaun Trial)"  | tee -a log-install.txt
-echo "hapus (Menghapus Akaun SSH)"  | tee -a log-install.txt
-echo "login (Semak login user)"  | tee -a log-install.txt
-echo "member (Senarai user)"  | tee -a log-install.txt
-echo "resvis (Restart Service dropbear, webmin, squid3, openvpn dan ssh)"  | tee -a log-install.txt
-echo "reboot (Reboot VPS)"  | tee -a log-install.txt
-echo "speedtest (Speedtest VPS)"  | tee -a log-install.txt
-echo "about (Informasi tentang script auto install)"  | tee -a log-install.txt
-echo ""  | tee -a log-install.txt
-echo "Feature lain"  | tee -a log-install.txt
-echo "----------"  | tee -a log-install.txt
-echo "Webmin   : http://$MYIP:10000/"  | tee -a log-install.txt
-echo "Timezone : Asia/Malaysia (GMT +8)"  | tee -a log-install.txt
-echo "IPv6     : [off]"  | tee -a log-install.txt
-echo ""  | tee -a log-install.txt
-echo "Modified by Aiman Amir"  | tee -a log-install.txt
-echo ""  | tee -a log-install.txt
-echo "Log Instalasi --> /root/log-install.txt"  | tee -a log-install.txt
-echo ""  | tee -a log-install.txt
-echo "VPS AUTO REBOOT TIAP 12 JAM"  | tee -a log-install.txt
-echo ""  | tee -a log-install.txt
-echo "==========================================="  | tee -a log-install.txt
+
+echo "OpenSSH  : 22, 80"  
+echo "Dropbear : 443, 143" 
+echo "OpenVPN  : TCP 1194 (client config : http://$MYIP:85/client.ovpn)" 
+echo "nginx    : 85"  | tee -a log-install.txt
 cd
-printf '\n\nคุณจำเป็นต้องรีสตาร์ทระบบหนึ่งรอบ (y/n):'
+printf 'nคุณจำเป็นต้องรีสตาร์ทระบบหนึ่งรอบ (y/n):'
 read a
 if [ $a == 'y' ]
 then
