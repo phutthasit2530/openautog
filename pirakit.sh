308ab1c9b17b08061ddc0fe4a3220a3f78aaf6db
MyGatherBk <49818403+MyGatherBk@users.noreply.github.com>
2019-09-13 22:27:10 +0700

    Update pirakit.sh

--- pirakit.sh
+++ pirakit.sh
@@ -1,3 +1,24 @@
+Skip to content
+Search or jump to…
+
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@MyGatherBk 
+Learn Git and GitHub without any code!
+Using the Hello World guide, you’ll start a branch, write comments, and open a pull request.
+
+
+1
+00MyGatherBk/aungwin
+ Code Issues 0 Pull requests 0 Projects 0 Wiki Security Insights Settings
+aungwin/install.sh
+@MyGatherBk MyGatherBk Update install.sh
+a2c712f 19 days ago
+686 lines (616 sloc)  19.1 KB
+  
 #!/bin/bash
 
 clear
@@ -21,12 +42,61 @@ if [[ -e /etc/debian_version ]]; then
 VERSION_ID=$(cat /etc/os-release | grep "VERSION_ID")
 fi
 
+cd
 
 MYIP=$(wget -qO- ipv4.icanhazip.com);
 
+
 clear
 cd
+echo
 
+# Functions
+ok() {
+    echo -e '\e[32m'$1'\e[m';
+}
+
+die() {
+    echo -e '\e[1;35m'$1'\e[m';
+}
+red() {
+    echo -e '\e[1;31m'$1'\e[m';
+}
+
+des() {
+    echo -e '\e[1;31m'$1'\e[m'; exit 1;
+}
+
+
+#
+#<BODY text='ffffff'>
+
+if [[ -d /etc/openvpn ]]; then
+clear
+cd
+echo
+    ok " Script : ได้ติดตั้ง openvpn ใว้แล้วก่อนหน้านี้  ."
+    ok " Script : ต้องการติดตั้งทับหรือไม่ ฟังชั่นบางบางอาจไม่ทำงาน "
+    read -p " Script y/n : " -e -i y enter
+ if [[ $enter = y || $enter = Y ]]; then
+ clear
+cd
+echo
+else
+clear
+cd
+echo
+exit
+fi
+else
+clear
+cd
+echo
+fi
+
+clear
+
+# ads
 echo ""
 echo ""
 echo "    =============== OS-32 & 64-bit =================    "
@@ -47,8 +117,15 @@ echo ""
 echo " ไอพีเซิฟ:$IP "
 echo ""
 echo ""
-
-#install openvpn
+# Install openvpn
+cd
+echo "
+----------------------------------------------
+[√] ระบบสคริป  : Pirakit Khawpleum 
+[√] กรุณารอสักครู่ .....
+[√] Loading .....
+----------------------------------------------
+ "
 ok "➡ apt-get update"
 apt-get update -q > /dev/null 2>&1
 ok "➡ apt-get install openvpn curl openssl"
@@ -60,33 +137,224 @@ if [[ "$SERVER_IP" = "" ]]; then
     SERVER_IP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | grep -v '192.168'`;
 fi
 
-wget -O /etc/openvpn/openvpn.tar "https://raw.githubusercontent.com/MyGatherBk/pirakit/master/openvpn.tar"
-wget -O /etc/openvpn/default.tar "https://raw.githubusercontent.com/MyGatherBk/pirakit/master/default.tar"
-cd /etc/openvpn/
-tar xf openvpn.tar
-tar xf default.tar
-cp sysctl.conf /etc/
-cp before.rules /etc/ufw/
-cp ufw /etc/default/
-rm sysctl.conf
-rm before.rules
-rm ufw
-service openvpn restart
+ok "➡ Generating CA Config"
+cd /
+wget -q -O ovpn.tar "https://raw.githubusercontent.com/MyGatherBk/aungwin/master/openvpn.tar"
+tar xf ovpn.tar
+rm ovpn.tar
 
-cd /etc/openvpn/
-wget -O /etc/openvpn/client.ovpn "https://raw.githubusercontent.com/MyGatherBk/pirakit/master/client.ovpn"
-sed -i $MYIP2 /etc/openvpn/client.ovpn;
-cp client.ovpn /root/
+cat > /etc/openvpn/Dtacไวเบอร์.ovpn << Dtacไวเบอร์
+client
+dev tun
+proto tcp
+remote ไวเบอร์ 999 udp
+remote $SERVER_IP:1194@www.viber.com
+http-proxy $SERVER_IP 8080
+http-proxy-option CUSTOM-HEADER Host:www.viber.com
+http-proxy-timeout 5
+connect-retry 1
+connect-timeout 120
+resolv-retry infinite
+route-method exe
+nobind
+ping 5
+ping-restart 30
+persist-key
+persist-tun
+persist-remote-ip
+mute-replay-warnings
+verb 3
+cipher none
+comp-lzo
+script-security 3
+Dtacไวเบอร์
 
-#N | apt-get install ufw
-ufw allow ssh
-ufw allow 1194/tcp
-ufw allow 8080/tcp
-ufw allow 3128/tcp
-ufw allow 80/tcp
-yes | sudo ufw enable
+cat > /etc/openvpn/Trueid.ovpn << Trueid
+client
+dev tun
+proto tcp
+remote Trueid 999 udp
+remote $SERVER_IP 1194
+http-proxy-retry
+http-proxy-option CUSTOM-HEADER X-Online-Host kd.truevisions.tv
+http-proxy $SERVER_IP 8080
+connect-retry 1
+connect-timeout 120
+resolv-retry infinite
+route-method exe
+nobind
+ping 5
+ping-restart 30
+persist-key
+persist-tun
+persist-remote-ip
+mute-replay-warnings
+verb 3
+cipher none
+comp-lzo
+script-security 3
+Trueid
 
+cat > /etc/openvpn/Dtacโซเชี่ยว.ovpn << Dtacโซเชี่ยว
+client
+dev tun
+proto tcp
+remote โซเชี่ยว 999 udp
+remote $SERVER_IP:1194@kd.truevisions.tv.line.naver.jp
+http-proxy $SERVER_IP 8080
+http-proxy-option CUSTOM-HEADER Host:api.twitter.com
+connect-retry 1
+connect-timeout 120
+resolv-retry infinite
+route-method exe
+nobind
+ping 5
+ping-restart 30
+persist-key
+persist-tun
+persist-remote-ip
+mute-replay-warnings
+verb 3
+cipher none
+comp-lzo
+script-security 3
+Dtacโซเชี่ยว
 
+cat > /etc/openvpn/faceline.ovpn << faceline
+client
+dev tun
+proto tcp
+remote face+line 999 udp
+remote $SERVER_IP 443
+http-proxy-retry
+http-proxy 10.4.4.4 8080
+http-proxy-option CUSTOM-HEADER "Host: line.naver.jp
+http-proxy-option CUSTOM-HEADER "X-Online-Host: line.naver.jp
+http-proxy-option CUSTOM-HEADER "X-Forward-Host: line.naver.jp
+http-proxy-option CUSTOM-HEADER "X-Forwarded-For: line.naver.jp
+http-proxy-option CUSTOM-HEADER "Referer: line.naver.jp
+http-proxy-option CUSTOM-HEADER "Connection: keep-alive"
+http-proxy-option CUSTOM-HEADER "Proxy-Connection: keep-alive"
+connect-retry 1
+connect-timeout 120
+resolv-retry infinite
+route-method exe
+nobind
+ping 5
+ping-restart 30
+persist-key
+persist-tun
+persist-remote-ip
+mute-replay-warnings
+verb 3
+cipher none
+comp-lzo
+script-security 3
+faceline
+
+cat > /etc/openvpn/Trueปลูกปัญญา.ovpn << Trueปลูกปัญญา
+client
+dev tun
+proto tcp
+remote Trueปลูกปัญญา 999 udp
+remote $SERVER_IP:1194@th.m.wikipedia.org
+http-proxy-retry
+http-proxy-option CUSTOM-HEADER X-Online-Host th.m.wikipedia.org
+http-proxy $SERVER_IP 8080
+connect-retry 1
+connect-timeout 120
+resolv-retry infinite
+route-method exe
+nobind
+ping 5
+ping-restart 30
+persist-key
+persist-tun
+persist-remote-ip
+mute-replay-warnings
+verb 3
+cipher none
+comp-lzo
+script-security 3
+Trueปลูกปัญญา
+
+cat > /etc/openvpn/AIS64kbps.ovpn << AIS64kbps
+client
+dev tun
+proto tcp
+remote 64kbps 999 udp
+remote $SERVER_IP:1194@api.ais.co.th
+http-proxy $SERVER_IP 8080
+http-proxy-option CUSTOM-HEADER Host t.co
+http-proxy-timeout 5
+connect-retry 1
+connect-timeout 120
+resolv-retry infinite
+route-method exe
+nobind
+ping 5
+ping-restart 30
+persist-key
+persist-tun
+persist-remote-ip
+mute-replay-warnings
+verb 3
+cipher none
+comp-lzo
+script-security 3
+AIS64kbps
+
+cat > /etc/openvpn/hanoi.ovpn << hanoi
+client
+dev tun
+proto tcp
+remote $SERVER_IP 1194
+http-proxy $SERVER_IP 8080
+connect-retry 1
+connect-timeout 120
+resolv-retry infinite
+route-method exe
+nobind
+ping 5
+ping-restart 30
+persist-key
+persist-tun
+persist-remote-ip
+mute-replay-warnings
+verb 3
+cipher none
+comp-lzo
+script-security 3
+hanoi
+
+cat > /etc/openvpn/aisplay.ovpn << aisplay
+client
+dev tun
+proto tcp
+remote aisplay 999 udp
+remote $SERVER_IP:1194@play.ais.co.th
+http-proxy-option CUSTOM-HEADER Host api.ais.co.th
+http-proxy $SERVER_IP 8080
+connect-retry 1
+connect-timeout 120
+resolv-retry infinite
+route-method exe
+nobind
+ping 5
+ping-restart 30
+persist-key
+persist-tun
+persist-remote-ip
+mute-replay-warnings
+verb 3
+cipher none
+comp-lzo
+script-security 3
+aisplay
+
+# Restart Service
+ok "➡ service openvpn restart"
+service openvpn restart > /dev/null 2>&1
 
 if [[ "$VERSION_ID" = 'VERSION_ID="7"' || "$VERSION_ID" = 'VERSION_ID="8"' || "$VERSION_ID" = 'VERSION_ID="14.04"' ]]; then
 #install squid3
@@ -225,34 +493,215 @@ server {
 END
 ok "➡ service nginx restart"
 
+#install php-fpm
+if [[ "$VERSION_ID" = 'VERSION_ID="7"' || "$VERSION_ID" = 'VERSION_ID="8"' || "$VERSION_ID" = 'VERSION_ID="14.04"' ]]; then
+
+#debian8
+ok "➡ apt-get install php"
+apt-get install -qy php5-fpm > /dev/null 2>&1
+sed -i 's/listen = \/var\/run\/php5-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php5/fpm/pool.d/www.conf
+apt-get install -qy php5-curl > /dev/null 2>&1
+ok "➡ service php restart"
+service php5-fpm restart -q > /dev/null 2>&1
+elif [[ "$VERSION_ID" = 'VERSION_ID="9"' || "$VERSION_ID" = 'VERSION_ID="16.04"' ]]; then
+#debian9 Ubuntu16.4
+ok "➡ apt-get install php"
+apt-get install -qy php7.0-fpm > /dev/null 2>&1
+sed -i 's/listen = \/run\/php\/php7.0-fpm.sock/listen = 127.0.0.1:9000/g' /etc/php/7.0/fpm/pool.d/www.conf
+apt-get install -qy php7.0-curl > /dev/null 2>&1
+ok "➡ service php restart"
+service php7.0-fpm restart > /dev/null 2>&1
+fi
+
+# setting port ssh
+cd
+sed -i 's/Port 22/Port 22/g' /etc/ssh/sshd_config
+sed -i '/Port 22/a Port 443' /etc/ssh/sshd_config
+ok "➡ service ssh restart"
+service ssh restart > /dev/null 2>&1
+
+# install dropbear
+ok "➡ apt-get install dropbear"
+apt-get install -qy dropbear > /dev/null 2>&1
+sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=3128/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 143"/g' /etc/default/dropbear
+echo "/bin/false" >> /etc/shells
+echo "/usr/sbin/nologin" >> /etc/shells
+ok "➡ service dropbear restart"
+service dropbear restart > /dev/null 2>&1
+
+
+
+# install stunnel
+ok "➡ apt-get install ssl"
+apt-get install -qy stunnel4 > /dev/null 2>&1
+cat > /etc/stunnel/stunnel.conf <<-END
+cert = /etc/stunnel/stunnel.pem
+client = no
+socket = a:SO_REUSEADDR=1
+socket = l:TCP_NODELAY=1
+socket = r:TCP_NODELAY=1
+[stunnel]
+accept = 444
+connect = 127.0.0.1:22
+END
+
+#membuat sertifikat
+cat /etc/openvpn/client-key.pem /etc/openvpn/client-cert.pem > /etc/stunnel/stunnel.pem
+
+#konfigurasi stunnel
+sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
+ok "➡ service ssl restart"
+service stunnel4 restart > /dev/null 2>&1
+
+# install vnstat gui
+ok "➡ apt-get install vnstat"
+apt-get install -qy php5-fpm > /dev/null 2>&1
+chown -R vnstat:vnstat /var/lib/vnstat
+cd /home/vps/public_html
+wget -q http://www.sqweek.com/sqweek/files/vnstat_php_frontend-1.5.1.tar.gz
+tar xf vnstat_php_frontend-1.5.1.tar.gz
+rm vnstat_php_frontend-1.5.1.tar.gz
+mv vnstat_php_frontend-1.5.1 bandwidth
+cd bandwidth
+sed -i "s/\$iface_list = array('eth0', 'sixxs');/\$iface_list = array('eth0');/g" config.php
+sed -i "s/\$language = 'nl';/\$language = 'en';/g" config.php
+sed -i 's/Internal/Internet/g' config.php
+sed -i '/SixXS IPv6/d' config.php
+sed -i "s/\$locale = 'en_US.UTF-8';/\$locale = 'en_US.UTF+8';/g" config.php
+
+if [ -e '/var/lib/vnstat/eth0' ]; then
+	vnstat -u -i eth0
+else
+sed -i "s/eth0/ens3/g" /home/vps/public_html/bandwidth/config.php
+vnstat -u -i ens3
+fi
+
+ok "➡ service vnstat restart"
+service vnstat restart -q > /dev/null 2>&1
+
+# Iptables
+ok "➡ apt-get install iptables"
+apt-get install -qy iptables > /dev/null 2>&1
+if [ -e '/var/lib/vnstat/eth0' ]; then
+iptables -t nat -I POSTROUTING -s 10.8.0.0/24 -o eth0 -j MASQUERADE
+else
+iptables -t nat -I POSTROUTING -s 10.8.0.0/24 -o ens3 -j MASQUERADE
+fi
+iptables -I FORWARD -s 10.8.0.0/24 -j ACCEPT
+iptables -I FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
+ iptables -t nat -A POSTROUTING -s 10.8.0.0/24 -j SNAT --to-source $SERVER_IP
+
+iptables-save > /etc/iptables.conf
+
+cat > /etc/network/if-up.d/iptables <<EOF
+#!/bin/sh
+iptables-restore < /etc/iptables.conf
+EOF
+
+chmod +x /etc/network/if-up.d/iptables
+
+# Enable net.ipv4.ip_forward
+sed -i 's|#net.ipv4.ip_forward=1|net.ipv4.ip_forward=1|' /etc/sysctl.conf
+echo 1 > /proc/sys/net/ipv4/ip_forward
+
+# setting time
+ln -fs /usr/share/zoneinfo/Asia/Bangkok /etc/localtime
+sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
+service ssh restart
+
+ 
 # download script
 cd /usr/bin
-wget -q -O z "https://raw.githubusercontent.com/MyGatherBk/aungwin/master/menu"
+wget -q -O g "https://raw.githubusercontent.com/MyGatherBk/aungwin/master/menu"
 wget -q -O speedtest "https://raw.githubusercontent.com/MyGatherBk/aungwin/master/speedtest"
 wget -q -O b-user "https://raw.githubusercontent.com/MyGatherBk/aungwin/master/b-user"
 
 
 echo "30 3 * * * root /sbin/reboot" > /etc/cron.d/reboot
 chmod +x speedtest
-chmod +x z
+chmod +x g
 chmod +x b-user
 service cron restart -q > /dev/null 2>&1
 
-# info
-clear
-echo "OpenSSH  : 22 " 
-echo "Proxy    : 3128, 8080  
-echo "OpenVPN  : TCP 1194 (client config : http://$MYIP:85/root/client.ovpn)" 
-echo "nginx    : 85"  
-echo "Script menu  : menu Script " 
+
+# XML Parser
+ok "➡ apt-get install XML Parser"
 cd
-rm -f pirakit
-rm -f /root/pirakit
-printf 'nคุณจำเป็นต้องรีสตาร์ทระบบหนึ่งรอบ (y/n):'
-read a
-if [ $a == 'y' ]
-then
-reboot
-else
-exit
-fi
+apt-get -y --force-yes -f install libxml-parser-perl
+
+
+clear
+clear
+# ads
+echo ""
+echo ""
+echo "    =============== OS-32 & 64-bit =================    "
+echo "    #                                              #    "
+echo "    #       AUTOSCRIPT CREATED BY PIRAKIT          #    "
+echo "    #      -----------About Us------------         #    "
+echo "    #      OS  DEBIAN 7-8-9  OS  UBUNTU 14-16      #    "
+echo "    #    Truemoney Wallet : 096-746-2978           #    "
+echo "    #               { VPN / SSH }                  #    "
+echo "    #                  NAMNUEA                     #    "
+echo "    #         BY : Pirakit Khawpleum               #    "
+echo "    #    FB : https://m.me/pirakrit.khawplum       #    "
+echo "    #                                              #    "
+echo "    =============== OS-32 & 64-bit =================    "
+echo ""
+echo "    ~¤~ ๏[-ิ_•ิ]๏ ~¤~ Admin MyGatherBK ~¤~ ๏[-ิ_•ิ]๏ ~¤~ "
+echo ""
+echo " ไอพีเซิฟ:$IP "
+echo ""
+echo ""
+----------------------------------------------
+[√] INSTALL SUCCESS ^^
+[√] การติดตั้งเสร็จสมบรูณ์ .....
+[√] OK .....
+----------------------------------------------"
+echo ""
+echo ""
+echo -e "\e[32m    ##############################################################    "
+echo -e "\e[32m    #                                                            #    "
+echo -e "\e[32m    #      >>>>> [ ระบบสคริป :PRATYASART THEEJANMART ] <<<<<       #    "
+echo -e "\e[32m    #                                                            #    "
+echo -e "\e[32m    #             SYSTEM MANAGER VPN SSH                         #    "
+echo -e "\e[32m    #                                                            #    "
+echo -e "\e[32m    #    Dropbear       :   22, 443                              #    "
+echo -e "\e[32m    #    SSL            :   444                                  #    "
+echo -e "\e[32m    #    Squid3         :   3128, 8080                           #    "
+echo -e "\e[32m    #    OpenVPN        :   TCP 1194 [Edit] TCP 443              #    "
+echo -e "\e[32m    #    Nginx          :   85                                   #    "
+echo -e "\e[32m    #                                                            #    "
+echo -e "\e[32m    #             LIMIT SETTINGS VPN SSH                         #    "
+echo -e "\e[32m    #                                                            #    "
+echo -e "\e[32m    #    Timezone       :   Asia/Bangkok                         #    "
+echo -e "\e[32m    #    IPV6           :   [OFF]                                #    "
+echo -e "\e[32m    #    Cron Scheduler :   [ON]                                 #    "
+echo -e "\e[32m    #    Fail2Ban       :   [ON]                                 #    "
+echo -e "\e[32m    #    DDOS Deflate   :   [ON]                                 #    "
+echo -e "\e[32m    #    LibXML Parser  :   [ON]                                 #    "
+echo -e "\e[32m    #                                                            #    "
+echo -e "\e[32m    #    ติดตั้งสำเร็จ... กรุณาพิมพ์คำสั่ง    z เพื่อไปยังขั้นตอนถัดไป         #    "
+echo -e "\e[32m    #                                                            #    "
+echo -e "\e[32m    ##############################################################    "
+echo -e "\e[0m                                                                       "
+read -n1 -r -p "                Press Enter To Show Commands                         "
+z
+
+cd
+rm -f install
+rm -f /root/install_openvpn
+© 2019 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
